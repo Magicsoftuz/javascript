70edbb2f6df33ba0672a067559b8bc8df43a2f55
MagicSoft <magicsoft.uz@gmail.com>
2022-02-21 13:43:46 +0500

    operator precedence

--- script.js
+++ script.js
@@ -1,81 +1,29 @@
-// Minus matematik operatori (-)
-
-let now = 2034;
-
+const now = 2022;
 const ageUmid = now - 1996;
-const ageLinda = now - 1999; // 2034 yilini ikki marta yozish urniga uni bitta variable ga olib quyamiz
+const ageLinda = now - 1999;
 
-// console.log(ageUmid);
-// console.log(ageLinda);
+// const bigAge = ageUmid > ageLinda;
 
-//------------------------------------------------------
+// Bu yerda birinchi bulib minus (-) operatori ishlaydi keyin taqqoslash (>) operatori ishlaydi.
+const bigAge = now - 1996 > now - 1999;
 
-// ikki ta console.log() yozish urniga bittada ham yozishimiz mumkin. buning uchun varibalelarni (,) vergul bilan ajratamiz
-console.log(ageUmid, ageLinda);
+let x, y;
+x = y = 25 - 14 - 5; // x = y = 6
 
-//------------------------------------------------------
+console.log(x, y);
 
-// ko'paytirish matematik operatori (*)
+const averageAge = ageLinda + ageUmid / 2;
+console.log(averageAge);
 
-const kopaytma = ageUmid * ageLinda;
-console.log(kopaytma);
+/* 
+1. (a+b) - Guruhlash
+2. a++ , a-- oshib borish yoki kamayib borish
+3. ++a , --a oshib borish yoki kamayib borish
+4. typeof - turini kursatish
+5. a**b - darajaga kutarish
+6. a*b, a/b, a%b - kupaytirish, bulish, qoldiqli bulish
+7. a+b, a-b - ayrish, qushish
+8. <, <=, >, >= - katta, kichik tenglinglari (taqqoslashlar)
+*/
 
-//------------------------------------------------------
-
-// Bo'lish matematik operatori (/)
-
-const bulish = ageUmid / ageLinda;
-console.log(bulish);
-
-//------------------------------------------------------
-
-// Darajaga kutarish 3 ni to'rtinchi darajasi = 3 * 3 * 3 * 3
-
-console.log(3 ** 4); // ikkita kupatirish beligisi darajaga kutaradi
-
-//------------------------------------------------------
-
-// Ikkita variable ni bir biriga qo'shish uchun (+) belgisi ishlatdik , agar buni string ga qulasak ikkita stringni birlashtirib beradi.
-const firstName = "Umid";
-const lastName = "Rustamov";
-console.log(firstName + lastName);
-
-// Agar ikkita suz urtasiga joy tashlamoqchi bulsak
-console.log(firstName + " " + lastName);
-
-//------------------------------------------------------
-
-// Assignment operators, Tenglik operatori (=)
-
-let x = 10 + 15;
-console.log(x);
-
-//ikkita operatorni bitta joyda kelishi (+=)
-
-x += 80; // x = x + 80;
-console.log(x);
-
-//--------------------------
-
-x *= 5; // x = x * 5;
-console.log(x);
-
-//--------------------------
-
-x++; // x = x + 1;
-console.log(x);
-
-//--------------------------
-
-x--; // x = x - 1;
-console.log(x);
-
-//------------------------------------------------------
-
-// Comparison operators, taqqoslash operatorlari
-console.log(ageUmid > ageLinda); // true qaytaradi chunki taqqoslash operatori tugri
-
-console.log(ageUmid >= 50); // false qaytaradi chunki ageumid = 38 ga teng. 38 >= 50  bu xato shuning uchun false.
-
-const isFullAge = ageLinda >= 30;
-console.log(isFullAge);
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
