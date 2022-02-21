70abd63d12f8dd2ac5312fcf9d784cacf0759dc6
MagicSoft <magicsoft.uz@gmail.com>
2022-02-21 10:48:01 +0500

    Values and Variables

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Umid"); // Umid - this is here Value
+console.log(40 + 5 + 3 + 3); //40,5,3,3 - Value
+
+let firstName = "Umid"; // firstName - variables    umid - value
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Har doim Variable ni yozayotganimizda camelCase dan foydalanamiz.
+
+//birinchi so'zning birinchi harfi har doim kichik harf bilan boshlanadi.
+// agar ikkinchi so'z qo'shmoqchi bo'lsak ikkinchi so'zning birinchi harfi katta harf bilan boshlanadi
+
+//name
+//last
+
+let lastName = "Rustamov";
+
+console.log(lastName);
+
+// variable lar har doim quydagicha boshlasligi kerak.
+
+//1. raqamlar bilan
+
+// let 3years = 3; - mumkin emas
+
+// 2. & - belgisi ishlatilmaydi.
+
+// let umid&Rustamov = "Umid"; - mumkin emas
+
+// 3. JavaScriptning o'zining so'zlari ishlatilmaydi.
+
+// let function = "University"; - mumkin emas
+
+// Function, name, new, package,return,long, ....
+
+// Har doim variable larga nom bermoqchi bulsak, mano anglatadigan nom berish kerak.
+
+let myFirstUniversity = "TATU";
+let mySecondUniversity = "NUU"; // shunaqa yozish afzal kuriladi.
+
+// let university1 = "TATU";
+// let university2 = "NUU";  // Bunday yozish afzal kurilmaydi.
