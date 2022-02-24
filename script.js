13e610ca2442f4e69b0880d1fddd3ac6292bb0f2
MagicSoft <magicsoft.uz@gmail.com>
2022-02-24 12:32:21 +0500

    Basic Array Operations

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-"use strict"; // Qoidalar asosida to'g'ri yozishga yordam beradi
+const friends = ["Tolib", "Jafar", "Aziz", "Azizullo"]; //Array yaratib oldim.
 
-function ekrangaChiqar() {
-  console.log("Hello world!");
-  console.log("Salom Dunyo!");
-  console.log("hi Umid!");
-}
+// Arrayga yangi element qushish -----------------
 
-ekrangaChiqar();
-ekrangaChiqar();
-ekrangaChiqar();
+// 1-usul. Push methodi orqali (Array oxiridan qushadi yangi elementni)
+friends.push("Jo'raqo'zi");
+console.log(friends);
 
-function yoshniHisobla(tugilganYil, hozirgiYil) {
-  console.log("Tugilgan yilni hisoblash:");
-  return hozirgiYil - tugilganYil;
-}
+// 2-usul. Unshift methodi orqali (Array boshidan qushadi yangi elementni)
+friends.unshift("Doston");
+console.log(friends);
 
-console.log(yoshniHisobla(1996, 2022));
-console.log(yoshniHisobla(2000, 2022));
-console.log(yoshniHisobla(2002, 2022));
+// Arraydagi elementni uchirish -----------------
+
+// 1-usul. Pop methodi orqali (Array oxiridagi elemetni uchiradi)
+friends.pop();
+console.log(friends);
+
+// qaysi element ni uchirilganini kurish uchun friends.pop() ni bitta uzgaruvchiga tenglab olamiz.
+const uchirilganElement = friends.pop();
+console.log(uchirilganElement);
+
+// 2-usul. Shift methodi orqali (Arrayni boshidagi element ni uchiradi)
+friends.shift();
+console.log(friends);
+
+// Arraydagi element qaysi o'rinda turganini kurish -----------------
+const urni = friends.indexOf("Tolib");
+console.log(urni); // "Tolib" elementi agar array da mavjud bulsa qaysi urinda turgani chiqarib beradi. Agar mavjud bulmasa har doim "-1" qaytaradi.
+
+// Arrayni ichida biz bergan element bor yoki yuqligini tekshirish
+const tekshir = friends.includes("Aziz"); // Bu method har doim boolean qiymat qaytaradi. Agar element mavjud bulsa arrayni ichida (True) yoki mavjud bulmasa (False)
