5e5156af992b2e36981b12c44a8f2e8d3368f362
MagicSoft <magicsoft.uz@gmail.com>
2022-02-24 12:53:29 +0500

    Introduction to Objects

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-const friends = ["Tolib", "Jafar", "Aziz", "Azizullo"]; //Array yaratib oldim.
+// Array ga uxshab bir nechta value larni bitta variable ga joylashtirishni boshqa usuli ham mavjud. Bu Objects deyiladi.
+const umidArray = [
+  "Umid",
+  "Rustamov",
+  2022 - 1996,
+  "Developer",
+  ["Tolib", "Jafar", "Aziz"],
+]; // Shuni endi Object ga utkazamiz.
 
-// Arrayga yangi element qushish -----------------
+const umidObject = {
+  firstName: "Umid",
+  lastName: "Rustamov",
+  age: 2022 - 1996,
+  job: "Developer",
+  friends: ["Tolib", "Jafar", "Aziz"],
+};
 
-// 1-usul. Push methodi orqali (Array oxiridan qushadi yangi elementni)
-friends.push("Jo'raqo'zi");
-console.log(friends);
-
-// 2-usul. Unshift methodi orqali (Array boshidan qushadi yangi elementni)
-friends.unshift("Doston");
-console.log(friends);
-
-// Arraydagi elementni uchirish -----------------
-
-// 1-usul. Pop methodi orqali (Array oxiridagi elemetni uchiradi)
-friends.pop();
-console.log(friends);
-
-// qaysi element ni uchirilganini kurish uchun friends.pop() ni bitta uzgaruvchiga tenglab olamiz.
-const uchirilganElement = friends.pop();
-console.log(uchirilganElement);
-
-// 2-usul. Shift methodi orqali (Arrayni boshidagi element ni uchiradi)
-friends.shift();
-console.log(friends);
-
-// Arraydagi element qaysi o'rinda turganini kurish -----------------
-const urni = friends.indexOf("Tolib");
-console.log(urni); // "Tolib" elementi agar array da mavjud bulsa qaysi urinda turgani chiqarib beradi. Agar mavjud bulmasa har doim "-1" qaytaradi.
-
-// Arrayni ichida biz bergan element bor yoki yuqligini tekshirish
-const tekshir = friends.includes("Aziz"); // Bu method har doim boolean qiymat qaytaradi. Agar element mavjud bulsa arrayni ichida (True) yoki mavjud bulmasa (False)
+// Object bilan Array ning asosiy farqi:
+// 1. Object da elementning turgan urni ahamiyatga ega emas. Chunki elementlarni nomi orqali chaqiramiz.
+// 2. Array da elementlarni turgan urni muhim, chunki elementni turgan urnini bilmasak, uni chaqirolmaymiz.
