158707f01f6c7a345986dfd78607419cf3cb2ad1
MagicSoft <magicsoft.uz@gmail.com>
2022-02-25 11:26:01 +0500

    Looping Backwards and Loops in Loops

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-// Array ga uxshab bir nechta value larni bitta variable ga joylashtirishni boshqa usuli ham mavjud. Bu Objects deyiladi.
-const umidArray = [
-  "Umid",
-  "Rustamov",
-  2022 - 1996,
-  "Developer",
-  ["Tolib", "Jafar", "Aziz"],
-]; // Shuni endi Object ga utkazamiz.
+// Biror array ni man 0 dan 5 gacha elementlarni emas buni
+//teskarisini yani 5 dan 0 gacha chiqarmoqchi bulsam siklni teskari aylantiraman
 
-const umidObject = {
-  firstName: "Umid",
-  lastName: "Rustamov",
-  age: 2022 - 1996,
-  job: "Developer",
-  friends: ["Tolib", "Jafar", "Aziz"],
-};
+const umid = ["Umid", "Rustamov", 26, "Devloper", "Tashkent"];
 
-// Object bilan Array ning asosiy farqi:
-// 1. Object da elementning turgan urni ahamiyatga ega emas. Chunki elementlarni nomi orqali chaqiramiz.
-// 2. Array da elementlarni turgan urni muhim, chunki elementni turgan urnini bilmasak, uni chaqirolmaymiz.
+// shu teskari chiqarmoqchiman yani: Tashkent, Devloper, 26, Rustamov, Umid
+
+// Bu tugri usuli.
+for (let i = 0; i < umid.length; i++) {
+  console.log(umid[i]);
+}
+
+// Teskari aylantirish usuli:
+for (let i = umid.length; i >= 0; i--) {
+  console.log(umid[i]);
+}
+
+// Agar biror bitta sikl ichida yana sikl yaratmoqchi bulsak bu ichma-ich sikl deyiladi.
+for (let i = 0; i < 7; i++) {
+  switch (i) {
+    case 0:
+      console.log("Dushanba");
+      break;
+    case 1:
+      console.log("Seshanba");
+      break;
+    case 2:
+      console.log("Chorshanba");
+      break;
+    case 3:
+      console.log("Payshanba");
+      break;
+    case 4:
+      console.log("Juma");
+      break;
+    case 5:
+      console.log("Shanba");
+      break;
+    case 6:
+      console.log("Yakshanba");
+      break;
+  }
+  for (let j = 1; j <= 3; j++) {
+    console.log(`Mashqlar-${j} ni bajarish kerak!`);
+  }
+}
