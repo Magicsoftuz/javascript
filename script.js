be92e38e9e87619b1022cfc653c8e1d5a75ca247
MagicSoft <magicsoft.uz@gmail.com>
2022-02-25 11:42:52 +0500

    The While loop

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-// Biror array ni man 0 dan 5 gacha elementlarni emas buni
-//teskarisini yani 5 dan 0 gacha chiqarmoqchi bulsam siklni teskari aylantiraman
+// While sikl operator bilan for bir xil ishlaydi.
 
 const umid = ["Umid", "Rustamov", 26, "Devloper", "Tashkent"];
 
-// shu teskari chiqarmoqchiman yani: Tashkent, Devloper, 26, Rustamov, Umid
-
-// Bu tugri usuli.
 for (let i = 0; i < umid.length; i++) {
   console.log(umid[i]);
 }
+console.log("------------------");
 
-// Teskari aylantirish usuli:
-for (let i = umid.length; i >= 0; i--) {
-  console.log(umid[i]);
+let rep = 0; // boshlanish nuqtasi
+while (rep < umid.length) {
+  // shart beramiz. shart true bulib turguncha while ni ichidagi commanda ishlab turadi. Qachon false bulsa tuxtatadi
+  console.log(umid[rep]);
+  rep++; //Bu qadami, buni quyishimiz shart, bulmasa sikl tuxtamaydi.
 }
 
-// Agar biror bitta sikl ichida yana sikl yaratmoqchi bulsak bu ichma-ich sikl deyiladi.
-for (let i = 0; i < 7; i++) {
-  switch (i) {
-    case 0:
-      console.log("Dushanba");
-      break;
-    case 1:
-      console.log("Seshanba");
-      break;
-    case 2:
-      console.log("Chorshanba");
-      break;
-    case 3:
-      console.log("Payshanba");
-      break;
-    case 4:
-      console.log("Juma");
-      break;
-    case 5:
-      console.log("Shanba");
-      break;
-    case 6:
-      console.log("Yakshanba");
-      break;
-  }
-  for (let j = 1; j <= 3; j++) {
-    console.log(`Mashqlar-${j} ni bajarish kerak!`);
-  }
+// for dan foydalanib bumaydigan holatlarda While dan foydalanishimiz mumkin buladi. Masalan bizda faqat shart mavjud, boshlanish va tugash nuqtasi yuq
+
+// Man hozir narda uyini ni yarataman //
+
+// Manga buning uchun random raqam chiqadigan variable kerak buladi. Yani 1 dan 6 gacha random raqam chiqsin
+
+let narda = Math.trunc(Math.random() * 6) + 1; // Math.random -> bu funksiya hisoblanadi. va 0 dan 1 gacha random qiymat qaytaradi.
+
+while (narda !== 6) {
+  console.log(`Sizning ballingiz: ${narda}`);
+  narda = Math.trunc(Math.random() * 6) + 1;
+  narda === 6
+    ? console.log(`Afsus ${narda} raqami chiqib qoldi`)
+    : console.log();
 }
+
+// Bu narda 6 ga teng bumaguncha qayta qayta tushsin dedim
